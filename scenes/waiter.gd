f3ef02f246f6ebc2b745ba190b8e1a8dd8dddbcd
Danny <79547297+Danh295@users.noreply.github.com>
2025-06-08 00:17:54 -0400

    Added waiter update to pick up fruit

--- scenes/waiter.gd
+++ scenes/waiter.gd
@@ -1,11 +1,10 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 var moving = false
+var current_fruit: String = ""
 
-#func _ready():
-	#print(get_path())
+@onready var sprite = $Sprite2D
 
 func _process(delta):
 	if moving:
@@ -16,3 +15,17 @@ func _process(delta):
 
 func start_moving():
 	moving = true
+
+func pick_up_fruit(fruit: String):
+	current_fruit = fruit
+	update_sprite_with_fruit(fruit)
+
+func update_sprite_with_fruit(fruit: String):
+	match fruit:
+		#"banana":
+			#sprite.texture = preload("res://assets/character_with_banana.png")
+		"blueberry":
+			print("updating sprite to blueberry")
+			sprite.texture = preload("res://assets/Seagull-with-blueberry.png")
+		#"orange":
+			#sprite.texture = preload("res://assets/character_with_orange.png")
