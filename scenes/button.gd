1abbb2bf5e24db78c6e62c21568d26f7ddae4526
Danny <79547297+Danh295@users.noreply.github.com>
2025-06-07 23:34:12 -0400

    Added waiter movement on button press and collision

--- scenes/button.gd
+++ scenes/button.gd
@@ -2,15 +2,10 @@ extends Button
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+func _ready():
+	var waiter = get_node("/root/Game/Waiter")  # Adjust this path!
+	self.pressed.connect(func (): waiter.start_moving())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_pressed() -> void:
-	print("pressed button")
-	pass # Replace with function body.
